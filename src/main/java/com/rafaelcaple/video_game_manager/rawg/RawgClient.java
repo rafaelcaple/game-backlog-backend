638898710b40e78412dabe287342368d212070ab
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-17 15:32:33 -0300

    rawg api

---
+++ src/main/java/com/rafaelcaple/video_game_manager/rawg/RawgClient.java
@@ -0,0 +1,34 @@
+package com.rafaelcaple.video_game_manager.rawg;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Component
+public class RawgClient {
+
+    private final WebClient webClient;
+
+    @Value("${rawg.api.key}")
+    private String apiKey;
+
+    public RawgClient (){
+        this.webClient = WebClient.builder()
+                .baseUrl("https://api.rawg.io/api")
+                .build();
+    }
+
+    public Map<String, Object> searchGames(String query) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/games")
+                        .queryParam("key",apiKey)
+                        .queryParam("search",query)
+                        .build())
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+    }
+}
