dece35f8107f45c5fbadd7c3ca7fbf70aba0f4a3
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-19 15:18:22 -0300

    added vercel to cors

--- src/main/java/com/rafaelcaple/video_game_manager/CorsConfig.java
+++ src/main/java/com/rafaelcaple/video_game_manager/CorsConfig.java
@@ -9,7 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://game-backlog-topaz.vercel.app"
+                )
                 .allowedMethods("GET","POST","PATCH","DELETE");
     }
 }
