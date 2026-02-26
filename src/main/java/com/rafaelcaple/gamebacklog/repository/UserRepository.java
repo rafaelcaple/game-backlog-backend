9d9085032df10e7c446da2be6702fddd66e2bc6a
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-26 14:08:24 -0300

    add UserRepository.java

---
+++ src/main/java/com/rafaelcaple/gamebacklog/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rafaelcaple.gamebacklog.repository;
+
+import com.rafaelcaple.gamebacklog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
