7905fe8f6923feffb57cf119b5af2bdbaed9ca34
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-26 14:49:51 -0300

    add user authentication to GameController

--- src/main/java/com/rafaelcaple/gamebacklog/controller/GameController.java
+++ src/main/java/com/rafaelcaple/gamebacklog/controller/GameController.java
@@ -1,10 +1,12 @@
 package com.rafaelcaple.gamebacklog.controller;
 
 import com.rafaelcaple.gamebacklog.entity.Game;
+import com.rafaelcaple.gamebacklog.entity.User;
 import com.rafaelcaple.gamebacklog.enums.GameEnums;
 import com.rafaelcaple.gamebacklog.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,24 +18,26 @@ import java.util.Map;
 public class GameController {
     private final GameService service;
 
-    @GetMapping
-    public ResponseEntity<List<Game>> listSaved (){
-        return ResponseEntity.ok(service.listSaved());
-    }
     @GetMapping("/search")
     public ResponseEntity<Map<String,Object>> searchGames (@RequestParam String query) {
         return ResponseEntity.ok(service.searchGames(query));
     }
+
+    @GetMapping
+    public ResponseEntity<List<Game>> listSaved (@AuthenticationPrincipal User user){
+        return ResponseEntity.ok(service.listSaved(user));
+    }
+
     @PostMapping("/save/{rawgId}")
-    public ResponseEntity<Game> saveFromRawg (@PathVariable Integer rawgId) {
-        return ResponseEntity.ok(service.saveFromRawg(rawgId));
+    public ResponseEntity<Game> saveFromRawg (@PathVariable Integer rawgId, @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(service.saveFromRawg(rawgId, user));
     }
     @PatchMapping("/{id}/status")
-    public ResponseEntity<Game> updateStatus (@PathVariable Long id, @RequestParam GameEnums.GameStatus status) {
-        return ResponseEntity.ok(service.updateStatus(id,status));
+    public ResponseEntity<Game> updateStatus (@PathVariable Long id, @RequestParam GameEnums.GameStatus status, @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(service.updateStatus(id,status,user));
     }
     @DeleteMapping("/{id}")
-    public void delete (@PathVariable Long id) {
-        service.delete(id);
+    public void delete (@PathVariable Long id, @AuthenticationPrincipal User user) {
+        service.delete(id,user);
     }
 }
