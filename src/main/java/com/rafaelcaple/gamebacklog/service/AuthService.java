e92657c8cf68f42f6fb629849fe6d877568d69a7
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-26 14:14:59 -0300

    add authservice

---
+++ src/main/java/com/rafaelcaple/gamebacklog/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.rafaelcaple.gamebacklog.service;
+
+import com.rafaelcaple.gamebacklog.entity.User;
+import com.rafaelcaple.gamebacklog.repository.UserRepository;
+import com.rafaelcaple.gamebacklog.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public String register (String username, String password) {
+        String hashedPassword = passwordEncoder.encode(password);
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(hashedPassword);
+        user = userRepository.save(user);
+        return jwtService.generateToken(user);
+    }
+
+    public String login (String username, String password) {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Senha incorreta!");
+        }
+        return jwtService.generateToken(user);
+    }
+
+}
