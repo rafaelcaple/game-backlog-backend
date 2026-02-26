90bbab2b9d1a664d26ed542eb05098efa1fccb89
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-26 14:20:43 -0300

    create AuthController

--- src/main/java/com/rafaelcaple/gamebacklog/controller/AuthController.java
+++ src/main/java/com/rafaelcaple/gamebacklog/controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.rafaelcaple.gamebacklog.controller;
 
+import com.rafaelcaple.gamebacklog.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+    public record AuthRequest (String username,String password){}
+
+    @PostMapping("/register")
+    public String register (@RequestBody AuthRequest request) {
+        return authService.register(request.username(), request.password());
+    }
+
+    @PostMapping("/login")
+    public String login (@RequestBody AuthRequest request) {
+        return authService.login(request.username(), request.password());
+    }
+
 }
