724a56e599a5dbb1426d1e60d485a6a2b45ba3c5
Rafael Caple <rafaelcaple@gmail.com>
2026-03-04 13:39:26 -0300

    add input validations to username and password

--- src/main/java/com/rafaelcaple/gamebacklog/controller/AuthController.java
+++ src/main/java/com/rafaelcaple/gamebacklog/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.rafaelcaple.gamebacklog.controller;
 
 import com.rafaelcaple.gamebacklog.service.AuthService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,15 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
-    public record AuthRequest (String username,String password){}
+    public record AuthRequest (
+            @NotBlank(message = "Username is required")
+            @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+            String username,
+
+            @NotBlank(message = "Password is required")
+            @Size(min = 8, max = 72, message = "Password must be between 8 and 72 characters")
+            String password
+    ){}
 
     @PostMapping("/register")
-    public String register (@RequestBody AuthRequest request) {
+    public String register (@Valid @RequestBody AuthRequest request) {
         return authService.register(request.username(), request.password());
     }
 
     @PostMapping("/login")
-    public String login (@RequestBody AuthRequest request) {
+    public String login (@Valid @RequestBody AuthRequest request) {
         return authService.login(request.username(), request.password());
     }
 
